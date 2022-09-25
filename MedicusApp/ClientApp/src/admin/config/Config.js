9a8fc76531a7887edb2ad3875f2860665b6b5278
Mateusz Lapies <mat.lapies@gmail.com>
2022-09-26 01:00:38 +0200

    fix

--- MedicusApp/ClientApp/src/admin/config/Config.js
+++ MedicusApp/ClientApp/src/admin/config/Config.js
@@ -3,8 +3,8 @@ import config from './config.json';
 const isDev = !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
 
 if(!isDev) {
-    config.prod.url = window.location.protocol + "//" + config.prod.url.replace('medicus','') + window.location.hostname;
-    config.prod.minio = window.location.protocol + "//" + config.prod.minio.replace('medicus','') + window.location.hostname + "/";
+    config.prod.url = window.location.protocol + "//" + config.prod.url + window.location.hostname.replace('medicus','');
+    config.prod.minio = window.location.protocol + "//" + config.prod.minio + window.location.hostname.replace('medicus','') + "/";
 }
 
 const Config = isDev ? config.dev : config.prod;
