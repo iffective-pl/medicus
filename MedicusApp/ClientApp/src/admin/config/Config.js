e3d8a58470517b84a02518686e08570662125771
Mateusz Lapies <mat.lapies@gmail.com>
2022-09-26 19:44:17 +0200

    fix

--- MedicusApp/ClientApp/src/admin/config/Config.js
+++ MedicusApp/ClientApp/src/admin/config/Config.js
@@ -1,8 +1,19 @@
 import config from './config.json';
 
+function env() {
+    switch(window.location.hostname) {
+        default:
+        case "localhost":
+            return "Development"
+        case "medicus.iffective.pl":
+            return "Preprod"
+        case "medicus.wloclawek.pl":
+            return "Production"
+    }
+}
+
 function cfg() {
-    console.log(process.env)
-    switch(process.env.NODE_ENV) {
+    switch(env()) {
         default:
         case "Development":
             return config.Development
