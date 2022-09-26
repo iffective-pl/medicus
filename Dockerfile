8e30fd400fcc38f6f1bd142159d093ff4c5bd861
Mateusz Lapies <mat.lapies@gmail.com>
2022-09-26 13:30:38 +0200

    fix

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,8 @@
 FROM mcr.microsoft.com/dotnet/sdk:6.0 AS build-env
+ARG NODE_ENV
+ARG ASPNETCORE_ENVIRONMENT
+ENV NODE_ENV ${NODE_ENV}
+ENV ASPNETCORE_ENVIRONMENT ${ASPNETCORE_ENVIRONMENT}
 RUN curl -fsSL https://deb.nodesource.com/setup_16.x | bash -
 RUN apt-get install -y nodejs
 WORKDIR /app
--- MedicusApp/ClientApp/src/admin/config/Config.js
+++ MedicusApp/ClientApp/src/admin/config/Config.js
@@ -1,17 +1,17 @@
 import config from './config.json';
 
-const isDev = !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
-
-if(!isDev) {
-    if(window.location.hostname === "medicus.iffective.pl") {
-        config.prod.url = window.location.protocol + "//" + config.prod.url + window.location.hostname.replace('medicus','');
-        config.prod.minio = window.location.protocol + "//" + config.prod.minio + window.location.hostname.replace('medicus','') + "/";
-    } else {
-        config.prod.url = window.location.protocol + "//" + config.prod.url + window.location.hostname;
-        config.prod.minio = window.location.protocol + "//" + config.prod.minio + window.location.hostname + "/";
+function cfg() {
+    switch(process.env.NODE_ENV) {
+        default:
+        case "dev":
+            return config.dev
+        case "preprod":
+            return config.preprod
+        case "prod":
+            return config.prod
     }
 }
 
-const Config = isDev ? config.dev : config.prod;
+const Config = cfg();
 
 export default Config;
